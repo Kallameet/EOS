456ecb9bb5522ed1b05082d125219524367772d5
Reinhard <reinhard93@gmx.at>
2014-11-18 19:56:06 +0100

    ue3 fixed

--- Uebung3/ue3-files/LedDriver/ledDriver.c
+++ Uebung3/ue3-files/LedDriver/ledDriver.c
@@ -5,6 +5,7 @@
 #include <asm/io.h>
 #include <linux/cdev.h>
 #include "gpio_defs.h"
+#include "hwaccess.h"
 
 #define DEVICE_NAME "leds"
 #define MINOR_NUMBER 0
@@ -12,69 +13,74 @@
 
 MODULE_LICENSE("Dual BSD/GPL");
 
-struct file_operations ledDriver_fops = {
-    read:     ledDriver_read,
-    write:    ledDriver_write    
-};
 
-gpio_dev_t gpio_dev;
-cdev device;
-dev_t dev;
+struct gpio_dev_t gpio_dev;
+
+static ssize_t ledDriver_read(struct file *filp,	/* see include/linux/fs.h   */
+	char *buffer,	/* buffer to fill with data */
+	size_t length,	/* length of the buffer     */
+	loff_t * offset)
+{
+	buffer[0] = readSwitches(gpio_dev.vaddr);
+	return 1;
+}
+
+static ssize_t ledDriver_write(struct file *filp, const char *buffer, size_t length, loff_t * offset)
+{
+	setLeds(gpio_dev.vaddr, buff[0]);
+	return 0;
+}
+
+struct file_operations ledDriver_fops = {
+	read:    ledDriver_read,
+	write:	 ledDriver_write
+};
 
 static int ledDriver_init(void)
 {
-		if (request_mem_region(GPIO_BASE, sizeof(GpioRegs), DEVICE_NAME) == NULL)
-		{
-		    printk(KERN_ALERT "Error requesting memory\n");			
-				return -1;		
-		}		
-		gpio_dev.vaddr = (GpioRegs*) ioremap_nocache(GPIO_BASE, sizeof(GpioRegs));
-		
-		if (alloc_chrdev_region(&dev, MINOR_NUMBER, NUMBER_OF_DEVICES, DEVICE_NAME))
-		{
-				goto fail_alloc_chrdev;
-		}
+	if (request_mem_region(GPIO_BASE, sizeof(GpioRegs), DEVICE_NAME) == NULL)
+	{
+		printk(KERN_ALERT "Error requesting memory\n");
+		return -1;
+	}
+	gpio_dev.vaddr = (GpioRegs*)ioremap_nocache(GPIO_BASE, sizeof(GpioRegs));
 
-		cdev_init(&device, &ledDriver_fops);
-		cdev.ops = &ledDriver_fops;
+	if (alloc_chrdev_region(&gpio_dev.first, MINOR_NUMBER, NUMBER_OF_DEVICES, DEVICE_NAME))
+	{
+		goto fail_alloc_chrdev;
+	}
 
-		if (cdev_add(&device, dev, NUMBER_OF_DEVICES))
-		{
-				goto fail_cdev_add;
-		}
+	cdev_init(&gpio_dev.cdev, &ledDriver_fops);
 
-    printk(KERN_ALERT "Hello, world\n");
-    return 0;
+	if (cdev_add(&gpio_dev.cdev, gpio_dev.first, NUMBER_OF_DEVICES) < 0)
+	{
+		goto fail_cdev_add;
+	}
+
+	initLeds(gpio_dev.vaddr);
+	initSwitches(gpio_dev.vaddr);
+
+	return 0;
+
+fail_cdev_add:
+	unregister_chrdev_region(gpio_dev.first, NUMBER_OF_DEVICES);
+
+fail_alloc_chrdev:
+	iounmap((void*)gpio_dev.vaddr);
+	release_mem_region(GPIO_BASE, sizeof(GpioRegs));
+
+	return -1;
 }
 
 static void ledDriver_exit(void)
 {
-		cdev_del(&device);	
+	cdev_del(&gpio_dev.cdev);
 
-fail_cdev_add:
-		unregister_chrdev_region(dev, NUMBER_OF_DEVICES);
+	unregister_chrdev_region(gpio_dev.first, NUMBER_OF_DEVICES);
 
-fail_alloc_chrdev:
-		iounmap(gpio_dev.vaddr);
-		release_mem_region(GPIO_BASE, sizeof(GpioRegs));
-    printk(KERN_ALERT "Goodbye, cruel world\n");
+	iounmap((void*)gpio_dev.vaddr);
+	release_mem_region(GPIO_BASE, sizeof(GpioRegs));
 }
-
-static ssize_t device_read(struct file *filp,	/* see include/linux/fs.h   */
-			   char *buffer,	/* buffer to fill with data */
-			   size_t length,	/* length of the buffer     */
-			   loff_t * offset)
-{
-	return 0;
-}
-
-
-static ssize_t device_write(struct file *filp, const char *buffer, size_t length, loff_t * offset)
-{	
-	return 0;
-}
-
 
 module_init(ledDriver_init);
 module_exit(ledDriver_exit);
-
