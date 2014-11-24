faae125633b70987fb1b86b8d3344a0487189a49
BastiR1991 <basti.ratzenboeck@gmx.at>
2014-11-24 21:16:55 +0100

    added Uebung4 completed

--- Uebung4/LedDriver_ue4/ledDriver.c
+++ Uebung4/LedDriver_ue4/ledDriver.c
@@ -1,5 +1,6 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/kernel.h>
 #include <linux/fs.h>
 #include <linux/ioport.h>
 #include <asm/io.h>
@@ -7,6 +8,7 @@
 #include "gpio_defs.h"
 #include "hwaccess.h"
 #include <asm/arch/irqs.h>
+#include <asm/uaccess.h>
 #include <linux/interrupt.h>
 
 #define DEVICE_NAME "leds"
@@ -17,96 +19,115 @@
 MODULE_LICENSE("Dual BSD/GPL");
 
 
-static ssize_t ledDriver_read(struct file *filp,	/* see include/linux/fs.h   */
-			   char *buffer,	/* buffer to fill with data */
-			   size_t length,	/* length of the buffer     */
-			   loff_t * offset)
+struct gpio_dev_t gpio_dev;
+static struct work_struct ledDriver_wq;
+
+static ssize_t ledDriver_read(struct file *filp, /* see include/linux/fs.h */
+							  char *buffer, /* buffer to fill with data */
+							  size_t length, /* length of the buffer */
+							  loff_t * offset)
 {
-	return 0;
+	buffer[0] = readSwitches(gpio_dev.vaddr);
+	return 1;
 }
 
 static ssize_t ledDriver_write(struct file *filp, const char *buffer, size_t length, loff_t * offset)
-{	
+{
+	setLeds(gpio_dev.vaddr, buffer[0]);
 	return 0;
 }
 
-
 struct file_operations ledDriver_fops = {
-    read:     ledDriver_read,
-    write:    ledDriver_write    
+read: ledDriver_read,
+write: ledDriver_write
 };
 
-struct gpio_dev_t gpio_dev;
+void ledDriver_do_tasklet(unsigned long unused) {
+	setLeds(gpio_dev.vaddr, 0x02); 
+	printk("Led 2 set\n"); 
+	setLeds(gpio_dev.vaddr, 0x00);
+}
+
+void ledDriver_do_wq(unsigned long unused) {
+	setLeds(gpio_dev.vaddr, 0x04);
+	printk("Led 3 set!\n"); 
+	setLeds(gpio_dev.vaddr, 0x00);
+}
+
+DECLARE_TASKLET(ledDriver_tasklet, ledDriver_do_tasklet, 0);
 
 irqreturn_t led_interrupt(int irq, void *dev_id, struct pt_regs *regs)
 {
-    setLeds(gpio_dev.vaddr, 0x01);
-    setLeds(gpio_dev.vaddr, 0x00);
+	setLeds(gpio_dev.vaddr, 0x01);
+	setLeds(gpio_dev.vaddr, 0x00);
 
-    return IRQ_HANDLED;
+	//tasklet_schedule(&ledDriver_tasklet);
+	
+	schedule_work(&ledDriver_wq);
+
+	return IRQ_HANDLED;
 }
-
 
 static int ledDriver_init(void)
 {
-		if (request_mem_region(GPIO_BASE, sizeof(GpioRegs), DEVICE_NAME) == NULL)
-		{
-		    printk(KERN_ALERT "Error requesting memory\n");			
-				return -1;		
-		}		
-		gpio_dev.vaddr = (GpioRegs*) ioremap_nocache(GPIO_BASE, sizeof(GpioRegs));
-		
-		if (alloc_chrdev_region(&gpio_dev.first, MINOR_NUMBER, NUMBER_OF_DEVICES, DEVICE_NAME))
-		{
-				//goto fail_alloc_chrdev;
-		}
+	if (request_mem_region(GPIO_BASE, sizeof(GpioRegs), DEVICE_NAME) == NULL)
+	{
+		printk(KERN_ALERT "Error requesting memory\n");
+		return -1;
+	}
+	gpio_dev.vaddr = (GpioRegs*)ioremap_nocache(GPIO_BASE, sizeof(GpioRegs));
 
-		cdev_init(&gpio_dev.cdev, &ledDriver_fops);
+	if (alloc_chrdev_region(&gpio_dev.first, MINOR_NUMBER, NUMBER_OF_DEVICES, DEVICE_NAME))
+	{
+		goto fail_alloc_chrdev;
+	}
 
-		if (cdev_add(&gpio_dev.cdev, gpio_dev.first, NUMBER_OF_DEVICES))
-		{
-				//goto fail_cdev_add;
-		}
-		
+	cdev_init(&gpio_dev.cdev, &ledDriver_fops);
 
-		initLeds(gpio_dev.vaddr);
-		initSwitches(gpio_dev.vaddr);
-/*
-		int ledIrq = SWITCH0_IRQ;
-		if (ledIrq >= 0) 
-		{
-			result = request_irq(ledIrq, led_interrupt, SA_INTERRUPT, DEVICE_NAME, NULL);
-			if (result) 
-			{
-				printk(KERN_INFO "led: can't get assigned irq %i\n", ledIrq);
-				ledIrq = -1;
-			}
-			else 
-			{ *//* actually enable it -- assume this *is* a parallel port */
-				/*outb(0x10,short_base+2);
-			}
-		}
+	if (cdev_add(&gpio_dev.cdev, gpio_dev.first, NUMBER_OF_DEVICES))
+	{
+		goto fail_cdev_add;
+	}
 
+
+	initLeds(gpio_dev.vaddr);
+	initSwitches(gpio_dev.vaddr);
+
+	INIT_WORK(&ledDriver_wq, (void (*)(void *))ledDriver_do_wq, NULL);
+
+	int ledIrq = SWITCH0_IRQ;
+	if (ledIrq >= 0)
+	{
 		set_irq_type(SWITCH0_IRQ, IRQT_RISING);
-*/
-    printk(KERN_ALERT "Hello, world\n");
-    return 0;
+		int result = request_irq(ledIrq, led_interrupt, SA_INTERRUPT, DEVICE_NAME, NULL);
+		if (result)
+		{
+			printk(KERN_INFO "led: can't get assigned irq %i\n", ledIrq);
+			ledIrq = -1;
+		}
+	}
+	return 0;
+
+fail_cdev_add:
+	unregister_chrdev_region(gpio_dev.first, NUMBER_OF_DEVICES);
+
+fail_alloc_chrdev:
+	iounmap((void*)gpio_dev.vaddr);
+	release_mem_region(GPIO_BASE, sizeof(GpioRegs));
+
+	return -1;
 }
 
 static void ledDriver_exit(void)
 {
-		cdev_del(&gpio_dev.cdev);	
+	cdev_del(&gpio_dev.cdev);
 
-//fail_cdev_add:
-		unregister_chrdev_region(gpio_dev.first, NUMBER_OF_DEVICES);
+	unregister_chrdev_region(gpio_dev.first, NUMBER_OF_DEVICES);
 
-//fail_alloc_chrdev:
-		iounmap(gpio_dev.vaddr);
-		release_mem_region(GPIO_BASE, sizeof(GpioRegs));
-    printk(KERN_ALERT "Goodbye, cruel world\n");
+	iounmap(gpio_dev.vaddr);
+	release_mem_region(GPIO_BASE, sizeof(GpioRegs));
 }
 
 
 module_init(ledDriver_init);
 module_exit(ledDriver_exit);
-
